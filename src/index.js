8bfac19b117976da6384bb3476c48a44a5816865
Tino Reyna <tinoreyna1984@gmail.com>
2022-04-03 01:01:22 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client"; // ahora se exige en React 18
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+/**
+ * Notar que ya no se invoca a la clase ReactDOM ni al metodo render
+ * como tradicionalmente se hacía hasta la version 17.
+ */
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
